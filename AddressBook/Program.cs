8e0a03d41bd6728640c228c2c3ef0e63da90f223
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-26 13:20:08 +0530

    uc1_create_contacts

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
 Console.WriteLine("Address Book!");
+CreateContact contact1 = new CreateContact();
+//contact1.AddPerson();
+//contact1.Display();
+
+
+while(true)
+{
+    Console.WriteLine("Enter the option : \n1)Add Contact \n2)Display contact");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            contact1.AddPerson();
+            break;
+        case 2:
+            contact1.Display();
+            break;
+        default:
+            Console.WriteLine("Please choose correct option");
+            break;
+    }
+
+}
+
